5fb2bd09d28786ddbc822848b1a42476eb2d2268
guidi1401 <amandaguidi2003@gmail.com>
2020-02-19 17:49:58 -0300

    Link exercicios index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head> 
+		<meta charset="UTF-8"/>
+		<title>Organizando atividades WEB</title>
+	</head>
+	<body>
+		<p> <h1> Pagina com links dos meus exercicios </h1> </p>
+		<hr />
+		<a href="20200219/barbearia_bootstrap"> 20200219 - Barbearia </a>
+	</body>
+</html>
